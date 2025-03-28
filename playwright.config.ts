c726f91f12ca1b1436cc7667df7e122184621b8f
MatzaBateExtraterestriiAntici <62056156+MatzaBateExtraterestriiAntici@users.noreply.github.com>
2025-03-28 11:02:53 +0100

    Several improvements (#1)
    * Several improvements
    * Test mail.yml changes
    * Force use of bash

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+const envVars = require('./config/environmentVars.json'); // ✅ Import JSON
+const testVars = require('./config/testingVars.json'); // ✅ Import JSON
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -18,18 +10,21 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? testVars.retry_count : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? testVars.workers_count : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-reports/html-report', open: 'never' }], // HTML report
+    ['json', { outputFile: 'playwright-reports/json-report/results.json' }] // JSON report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',  // 'on', 'retain-on-failure', or 'off'
   },
 
   /* Configure projects for major browsers */
